55d43583e6917285acb88fbeb289461d256a36a3
Muhammad0085 <49810197+Muhammad0085@users.noreply.github.com>
2019-05-21 21:32:20 +0500

    Create Introducing Images.py

---
+++ Introducing Images.py
@@ -0,0 +1,6 @@
+#loading images
+car_img = pygame.image.load("car1.png")
+car2_img = pygame.image.load("car2.png")
+road_img = pygame.image.load("road.jpg")
+crash_img = pygame.image.load("crash.png")
+logo = pygame.image.load("logo.png")
